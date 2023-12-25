c97c1b3719389326c0ae20b26814bbe155396451
AkiraQZ <vboyrkin@gmail.com>
2023-12-25 22:27:16 +0800

    feat: dns stats task complited

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  let dnsStats = {};
+  for (let i = 0; i < domains.length; i++) {
+    let domainParts = domains[i].split('.').reverse();
+    let currentDomain = '';
+    for (let a = 0; a < domainParts.length; a++) {
+      currentDomain += `.${domainParts[a]}`;
+      dnsStats[currentDomain] = (dnsStats[currentDomain] || 0) + 1;
+    }
+  }
+  return dnsStats;
 }
 
 module.exports = {
